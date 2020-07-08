90499cfc031a6ba8e9880f22c6c697c27a2b9be5
ice <ice@shuiyunqing-MacBook-Pro.local>
2020-07-08 15:14:03 +0800

    remove author

--- spring-boot-server-gradle/src/main/java/me/ice/controler/IndexController.java
+++ spring-boot-server-gradle/src/main/java/me/ice/controler/IndexController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author <a href="mailto:shuiyunqing@outlook.com">ice</a>
- */
+
 @RestController
 @RequestMapping("api")
 public class IndexController {
